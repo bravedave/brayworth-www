e9391ca0af85869534c034c41d2bc01c4ab656ac
bravedave <davbray@gmail.com>
2018-01-04 09:01:27 +1000

    pesky spammers

--- application/app/Exceptions/CaptchaRequired.php
+++ application/app/Exceptions/CaptchaRequired.php
@@ -10,4 +10,14 @@
 	*/
 NameSpace Exceptions;
 
-class CaptchaRequired extends Exception {}
+class CaptchaRequired extends Exception {
+    public function __construct($message = null, $code = 0, Exception $previous = null) {
+
+      $this->_text = sprintf( 'Captcha Required : %s', \Request::get()->getRemoteIP());
+
+      // make sure everything is assigned properly
+      parent::__construct( $this->_text, $code, $previous);
+
+    }
+
+}
