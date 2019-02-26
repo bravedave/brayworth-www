a8ffd70ba1cb5d0ac62a19bca0ab498b14a1b989
Dave Bray <david@brayworth.com.au>
2019-02-26 12:30:46 +1000

    added react build

---
+++ application/app/views/home/precache-manifest.f546c38c9ab0aaa7f6e8efb38e491f28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b04cbed14ae426d8b7710b27855484c1",
+    "url": "./static/media/iphone-profile.b04cbed1.jpg"
+  },
+  {
+    "revision": "cdee0cb3b247c9c709d18285f1c9ab7f",
+    "url": "./static/media/bg-1600.cdee0cb3.jpg"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "f3cfd18cd59aaf64a261",
+    "url": "./static/js/main.f3cfd18c.chunk.js"
+  },
+  {
+    "revision": "d12a66fbc2f064a5dcd8",
+    "url": "./static/js/2.d12a66fb.chunk.js"
+  },
+  {
+    "revision": "f3cfd18cd59aaf64a261",
+    "url": "./static/css/main.d028998e.chunk.css"
+  },
+  {
+    "revision": "d12a66fbc2f064a5dcd8",
+    "url": "./static/css/2.c26e3b86.chunk.css"
+  },
+  {
+    "revision": "6d3829922c62ef4604925fd8f004c1d2",
+    "url": "./index.html"
+  }
+];
